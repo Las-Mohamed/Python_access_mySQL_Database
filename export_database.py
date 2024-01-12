950d51ea6c6cc7848692f15e44e5e8379dc62ed2
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2024-01-12 10:07:54 +0100

    Update export_database.py

--- export_database.py
+++ export_database.py
@@ -1,7 +1,7 @@
 import os
 
 # Export datadase as SQL Dump with mysqldump
-os.system('mysqldump -u tiko -p --host mysql-5b2bc8dc-o277c36e9.database.cloud.ovh.net --port 20184 SockWorld > database_SockWorld.sql')
+os.system('mysqldump -u <user_name> -p --host <hostname> --port <listening_port> <database_name> > database_SockWorld.sql')
 
 # Import dump into database
-# os.system('mysql -p -u [user] [database] < backup-file.sql')
+# os.system('mysql -p -u <user> <database> < backup-file.sql')
